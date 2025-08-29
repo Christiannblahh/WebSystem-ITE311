4c07978c7c3f923df45da53019dcb7b3853b2c39
Christian Bermudez <christiannblahh@gmail.com>
2025-08-29 13:24:01 +0800

    Added Home controller, routes, and views for basic navigation

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,8 +1,24 @@
 <?php
+
 namespace App\Controllers;
 
-class Home extends BaseController {
-    public function index() {
-        return view('template');
+class Home extends BaseController
+{
+    // Homepage
+    public function index()
+    {
+        return view('index'); // looks for app/Views/home.php
+    }
+
+    // About page
+    public function about()
+    {
+        return view('about'); // looks for app/Views/about.php
+    }
+
+    // Contact page
+    public function contact()
+    {
+        return view('contact'); // looks for app/Views/contact.php
     }
 }
--- app/Views/template.php
+++ app/Views/template.php
@@ -7,29 +7,32 @@
 </head>
 <body>
 
-    <!-- Simple Bootstrap Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">My Web System</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
-                    </li>
-                </ul>
-            </div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">My Web System</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <!-- Push nav links to the right -->
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link <?= service('uri')->getPath() == '' ? 'active' : '' ?>" href="<?= site_url('/') ?>">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?= service('uri')->getPath() == 'about' ? 'active' : '' ?>" href="<?= site_url('about') ?>">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?= service('uri')->getPath() == 'contact' ? 'active' : '' ?>" href="<?= site_url('contact') ?>">Contact</a>
+                </li>
+            </ul>
         </div>
-    </nav>
+    </div>
+</nav>
 
     <div class="container mt-4">
-        <h1>Welcome to CodeIgniter with Bootstrap</h1>
-        
+        <?= $this->renderSection('content') ?>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
