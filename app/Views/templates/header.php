a73edd1238aeeb87a69bfc8a40cccbc94a0ce7bc
Christian Bermudez <christiannblahh@gmail.com>
2025-09-25 22:02:37 +0800

    Admin Dashboard & Fixes

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -8,7 +8,7 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-        <a class="navbar-brand" href="<?= site_url('/dashboard') ?>">MyApp</a>
+        <a class="navbar-brand" href="<?= site_url('/dashboard') ?>">Learning Management System</a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
@@ -22,19 +22,28 @@
 
                 <?php if (session()->get('role') === 'student'): ?>
                     <li class="nav-item">
-                        <a class="nav-link" href="<?= site_url('/student/profile') ?>">My Profile</a>
+                        <a class="nav-link" href="<?= site_url('/dashboard') ?>">My Profile</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="<?= site_url('/student/profile') ?>">My Grades</a>
+                        <a class="nav-link" href="<?= site_url('/dashboard') ?>">My Grades</a>
                     </li>
                 <?php endif; ?>
 
                 <?php if (session()->get('role') === 'instructor'): ?>
                     <li class="nav-item">
-                        <a class="nav-link" href="<?= site_url('/instructor/profile') ?>">My Class</a>
+                        <a class="nav-link" href="<?= site_url('/dashboard') ?>">My Class</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="<?= site_url('/instructor/profile') ?>">Students</a>
+                        <a class="nav-link" href="<?= site_url('/dashboard') ?>">Students</a>
+                    </li>
+                <?php endif; ?>
+
+                <?php if (session()->get('role') === 'admin'): ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= site_url('/dashboard') ?>">Manage Users</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= site_url('/dashboard') ?>">Reports</a>
                     </li>
                 <?php endif; ?>
 
